65e62ab1bd66bc6dd6303533f0785505be1aedf6
Ramzey <remzesii@gmail.com>
2022-05-28 08:32:01 +0500

    Сделано первое задание

---
+++ ex001/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+void GetArray(){
+
+    
+    
+    int size = Convert.ToInt32(new Random().Next(0, 99));
+    int[] array = new int[size];
+    int count = 0;
+    
+
+    while (count < size){
+        int random = new Random().Next(100, 1000);
+        
+        array[count] = random;
+        count++;
+    }
+    Console.WriteLine();
+    Console.WriteLine("Был созан массив: ");
+    foreach (int i in array){
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+    
+    
+    Console.WriteLine("Четные числа массива: ");
+    int summ = 0;
+    foreach (int i in array){
+        
+        if (i%2 == 0){
+            summ = summ + i;  
+            Console.Write($"{i} ");
+        }
+    
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сумма четных элементов массива равна {summ}");
+    
+}
+
+GetArray();
+
